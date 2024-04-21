20a8b2605dc1e36b323a278785805f60187899c4
Яна Попова <ynappva@gmail.com>
2024-04-22 02:27:52 +0300

    src Main new

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 15_000;
         int rublesPerMile = 20;
-        int miles = amount / rublesPerMmile;
+        int miles = amount / rublesPerMile;
         System.out.println(miles);
     }
 }
